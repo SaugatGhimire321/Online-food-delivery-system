6412d06fe7d2669b00255fac3806c3cd05d4171b
Saugat <saugatghimire321@gmail.com>
2022-10-23 17:39:09 +0545

    signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,22 @@
 const User = require('../models/User');
 
 const returnSignupPage = (req, res) => {
+    res.render('signup')
 }
 
 const returnLoginPage = (req, res) => {
-    
+    res.render('login')
 }
 
 const createUser = async (req, res) => {
-    
+    console.log(req.body);
+    try{
+        const user = await User.create(req.body)
+        res.json({user:user._id})
+    }catch(err){
+        console.log(err)
+        res.json({errors:"Error occurred"})
+    }
 }
 
 const loginUser = (req, res) => {
